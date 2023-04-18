5fb8464a02212eefdee8aa0ae6cf6c86201a490e
Byron.C <byronrwcor@protonmail.com>
2023-04-18 14:09:58 -0700

    I think superstore.py is done. It might need some tweaking, though

--- scrapers/superstore.py
+++ scrapers/superstore.py
@@ -1,17 +1,26 @@
+from selenium.common.exceptions import ElementNotVisibleException
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import web_scraper as wb
+import web_scraper as scraper
+from time import sleep
+import csv
 
 #Defining constants
 BASE_URL = "https://www.realcanadiansuperstore.ca/"
-X_PATH_0 = "/html/body/div[1]/div/div[1]/div[3]/div/header/div[2]/div[1]/nav/ul/li[1]/button"
-X_PATH_1 = "/html/body/div[1]/div/div[2]/main/div/div/div/div[2]/div/div[2]/div[4]/div/ul"
+#The xpaths are for making sure their relevant pages load.
+MAIN_PAGE_TARGET = "primary-nav__list__item.primary-nav__list__item--with-children"
+SUB_PAGES_TARGET= "product-grid__results__products"
+COOKIES = {"name": "last_selected_store", "value": "1527"}
+DRIVER_OPTIONS = []
+DRIVER_PREFERENCES = [("permissions.default.image", 2), ("geo.enabled", True)]  #Preference 0 is for diabling images on webpages.
 
-#This block to to open the main page of the Superstore website and get the links for all the food departments.
-#It also creates the global-constant options for the browser.
-DRIVER_OPTIONS = ["--headless"]
-DRIVER_PREFERENCES = [("permissions.default.image", 2)]
-browser = pm.PageManager(BASE_URL, DRIVER_OPTIONS, DRIVER_PREFERENCES, X_PATH_0)
+#Lists
+links = []  #This is for the links pulled from the main page
+target_elements = []  #For the page source that is souped up belonging to each food department
+food_list = [["BRAND", "NAME", "PRICE"]]  #This is for the parsed soup data.
+
+#This block is to open the main page of the Superstore website and get the links for all the food departments.
+browser = scraper.PageManager(BASE_URL, DRIVER_OPTIONS, DRIVER_PREFERENCES, COOKIES, MAIN_PAGE_TARGET)
 page_content = browser.get_page_source()
 browser.close_driver()
 
@@ -19,28 +28,54 @@ browser.close_driver()
 soup = BeautifulSoup(page_content, 'html.parser')
 food_element = soup.find("ul", "primary-nav__list primary-nav__list--level-1")
 
-links = []
 for item in food_element:
     link = item.find('a', "primary-nav__list__item__link", href=True)
     links.append(link.get('href'))
 
 
-# y = len(links) - 1
+# y = len(links) - 2
 # while y > 0:
 #     links.pop()
 #     y -= 1
 
 loop = 0
-souped_content = []
-food_list = [["GROUP ", "NAME ", "PRICE "]]
 for item in links:
     item = str(item)
-    new_link = BASE_URL + item
-    print(f'Navigating to link num{loop}: ' + item)
-    browser = pm.PageManager(new_link, DRIVER_OPTIONS, DRIVER_PREFERENCES, X_PATH_1)
+    new_link = BASE_URL + item  #Create a new link for the browser.
+    print(f'Navigating to link num {loop}: ' + item)
+    browser = scraper.PageManager(new_link, DRIVER_OPTIONS, DRIVER_PREFERENCES, COOKIES, SUB_PAGES_TARGET)
+    loop = True
+    while loop:
+        try:
+            browser.click_element(".primary-button")
+        except:
+            loop = False
+        else:
+            continue
     page_source = browser.get_page_source()
-    souped_content = BeautifulSoup(page_source, 'html.parser')  #Convert the page source into usable string data
-    loop += 1
     browser.close_driver()
+    souped_content = BeautifulSoup(page_source, 'html.parser')  #Convert the page source into usable string data
+    target_elements.append(souped_content.find_all("div", class_="product-tile__details"))  #Gets the <ul> that holds the products
+    loop += 1
 
-print(links)
+
+for products in target_elements:
+    food_list.append([])
+    for element in products:
+        temp_list = []
+        name = element.find("span", "product-name__item product-name__item--name")
+        brand = element.find("span", "product-name__item product-name__item--brand")
+        price = element.find("span", "price__value selling-price-list__item__price selling-price-list__item__price--now-price__value")
+        #Get the wanted text values and makre sure they're not None
+        if not isinstance(name, type(None)):
+            temp_list.append(name.get_text())
+        if not isinstance(brand, type(None)):
+            temp_list.append(brand.get_text())
+        if not isinstance(price, type(None)):
+            temp_list.append(price.get_text())
+        food_list[-1].append(temp_list)
+
+
+with open("output.csv", "w", newline="") as f:
+    writer = csv.writer(f, delimiter="\n")
+    writer.writerows(food_list)
--- scrapers/web_scraper.py
+++ scrapers/web_scraper.py
@@ -3,26 +3,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 class PageManager():
-    def __init__(self,url, driver_options, driver_preferences, x_path):
-        self.x_path = x_path
+    def __init__(self,url, driver_options, driver_preferences, cookies, wait_target):
+        self.wait_target = wait_target
         self.url = url
         options = Options()
+        #Add any driver options if needed
         if driver_options:
             for item in driver_options:
                 options.add_argument(item)
+        #Add any profile preferences needed
         if driver_preferences:
             for item in driver_preferences:
                 options.set_preference(item[0], item[1])
         self.driver = webdriver.Firefox(options=options)
         self.driver.get(self.url)
+        #add any cookies that are needed
+        if cookies:
+            for item in cookies:
+                self.driver.add_cookie(cookies)
+        self.driver.refresh()
         print("Waiting")
         #Wait for the browser to load. The program waits until an element with a specific ID
-        elem = WebDriverWait(self.driver, 16).until(
-        EC.presence_of_element_located((By.XPATH, self.x_path))
-        )
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, self.wait_target))
+        WebDriverWait(self.driver, 30).until(element_present)
+        sleep(4)
         print("Done Waiting")
+
+    def click_element(self, click_target):
+        #This method clicks elements at the bottom of the page. Will probably need to change it scrolling only to the bottom
+        wait = WebDriverWait(self.driver, 10)
+        element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, click_target)))
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        element.click()
 
     def get_page_source(self):
         return self.driver.page_source
